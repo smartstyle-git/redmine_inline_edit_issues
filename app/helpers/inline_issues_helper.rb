f976d1455e010fa96b5aea88f631fd2836d9134d
Tomasz Gietek <tomasz.gietek@omegacode.pl>
2020-11-28 19:46:50 +0100

    v2.0.1

--- app/helpers/inline_issues_helper.rb
+++ app/helpers/inline_issues_helper.rb
@@ -1,23 +1,24 @@
 module InlineIssuesHelper
   include CustomFieldsHelper
+  include ProjectsHelper
 
   def inline_project_id
     @project.present? ? @project.id : ""
   end
-  
+
   def column_form_content(column, issue, f)
-    if(column.class.name == "QueryCustomFieldColumn")      
+    if column.class.name == "QueryCustomFieldColumn"
       custom_field_values = issue.editable_custom_field_values
-      value = custom_field_values.detect{|cfv| cfv.custom_field_id == column.custom_field.id}
-      custom_field_tag :issues, value, issue if value.present?
-    else 
+      value = custom_field_values.detect { |cfv| cfv.custom_field_id == column.custom_field.id }
+      custom_field_tag :issues, value, issue, f if value.present?
+    else
       case column.name
       when :tracker
-        f.select :tracker_id, issue.project.trackers.collect {|t| [t.name, t.id]}
+        f.select :tracker_id, issue.project.trackers.collect { |t| [t.name, t.id] }
       when :status
-        f.select :status_id, issue.new_statuses_allowed_to.collect {|p| [p.name, p.id]}
+        f.select :status_id, issue.new_statuses_allowed_to.collect { |p| [p.name, p.id] }
       when :priority
-        f.select :priority_id, @priorities.collect {|p| [p.name, p.id]}
+        f.select :priority_id, @priorities.collect { |p| [p.name, p.id] }
       when :subject
         f.text_field :subject, size: 20
       when :assigned_to
@@ -25,30 +26,30 @@ module InlineIssuesHelper
       when :estimated_hours
         f.text_field :estimated_hours, size: 3
       when :start_date
-        f.text_field(:start_date, size: 8) +
-        calendar_for('issues_'+issue.id.to_s+'_start_date') 
+        f.date_field(:start_date, size: 8) +
+            calendar_for('issues_' + issue.id.to_s + '_start_date')
       when :due_date
-        f.text_field(:due_date, size: 8) +
-        calendar_for('issues_'+issue.id.to_s+'_due_date')
-      when :done_ratio 
-        f.select :done_ratio, ((0..10).to_a.collect {|r| ["#{r*10} %", r*10] })
+        f.date_field(:due_date, size: 8) +
+            calendar_for('issues_' + issue.id.to_s + '_due_date')
+      when :done_ratio
+        f.select :done_ratio, ((0..10).to_a.collect { |r| ["#{r * 10} %", r * 10] })
       when :is_private
         f.check_box :is_private
       when :description
         f.text_area :description
       when :category
-        f.select :category_id, [["",""]] + issue.project.issue_categories.collect {|t| [t.name, t.id]}
+        f.select :category_id, [["", ""]] + issue.project.issue_categories.collect { |t| [t.name, t.id] }
       when :fixed_version
-        f.select :fixed_version_id, [["",""]] + issue.project.versions.collect {|t| [t.name, t.id]}
+        f.select :fixed_version_id, [["", ""]] + issue.project.versions.collect { |t| [t.name, t.id] }
       else
         column_display_text(column, issue)
-      end    
+      end
     end
   end
-  
-  def column_display_text(column, issue)    
+
+  def column_display_text(column, issue)
     value = column.value(issue)
-    
+
     case value.class.name
     when 'Time'
       format_time(value)
@@ -64,34 +65,34 @@ module InlineIssuesHelper
       h(value)
     end
   end
-  
+
   def column_total(column, issues)
     case column.name
     when :estimated_hours
       totalEstHours(issues)
     when :spent_hours
       totalSpentHours(issues)
-    end    
+    end
   end
-  
+
   def group_column_total(column, issues, group)
     case column.name
     when :estimated_hours
       totalGroupEstHours(issues, group)
     when :spent_hours
       totalGroupSpentHours(issues, group)
-    end 
+    end
   end
-  
+
   def inline_edit_condition
-    cond = "issues.id in (#{@ids.map{|i| i.to_i}.join(',')})"
+    cond = "issues.id in (#{@ids.map { |i| i.to_i }.join(',')})"
   end
-  
+
   def group_class_name(group)
     begin
       if group.present?
         # strip out white spaces from the group name
-        "group_" + group.name.gsub(/\s+/,"")
+        "group_" + group.name.gsub(/\s+/, "")
       else
         ""
       end
@@ -99,7 +100,7 @@ module InlineIssuesHelper
       ""
     end
   end
-  
+
   def group_total_name(group, column)
     if group.present? && column.present?
       "#{group_class_name(group)}_total_#{column.name}"
@@ -107,46 +108,46 @@ module InlineIssuesHelper
       ""
     end
   end
-  
+
   private
-  
+
   def totalEstHours(issues)
     estTotal = 0
-    issues.each {|i| estTotal += i.estimated_hours if i.estimated_hours.present?}
+    issues.each { |i| estTotal += i.estimated_hours if i.estimated_hours.present? }
     sprintf "%.2f", estTotal
   end
-  
+
   def totalSpentHours(issues)
     spentTotal = 0
-    issues.each {|i| spentTotal += i.spent_hours if i.spent_hours.present?}
+    issues.each { |i| spentTotal += i.spent_hours if i.spent_hours.present? }
     sprintf "%.2f", spentTotal
   end
-  
+
   def totalGroupEstHours(issues, group)
     estTotal = 0
-    issues.each do |i| 
-      if i.estimated_hours.present? &&  @query.group_by_column.value(i) == group
+    issues.each do |i|
+      if i.estimated_hours.present? && @query.group_by_column.value(i) == group
         estTotal += i.estimated_hours
       end
     end
     sprintf "%.2f", estTotal
   end
-  
+
   def totalGroupSpentHours(issues, group)
     spentTotal = 0
-    issues.each do |i| 
-      if i.spent_hours.present? &&  @query.group_by_column.value(i) == group
-        spentTotal += i.spent_hours 
+    issues.each do |i|
+      if i.spent_hours.present? && @query.group_by_column.value(i) == group
+        spentTotal += i.spent_hours
       end
     end
     sprintf "%.2f", spentTotal
   end
-  
-  
+
+
   #####
   # Return custom field html tag corresponding to its format
   #####
-  def custom_field_tag(name, custom_value, issue)
+  def custom_field_tag(name, custom_value, issue, f)
     custom_field = custom_value.custom_field
     field_name = "#{name}[#{issue.id}][custom_field_values][#{custom_field.id}]"
     field_name << "[]" if custom_field.multiple?
@@ -155,18 +156,28 @@ module InlineIssuesHelper
     tag_options = {:id => field_id, :class => "#{custom_field.field_format}_cf"}
 
     field_format = Redmine::FieldFormat.find(custom_field.field_format)
+
     case custom_field.field_format
+    when "attachment"
+      #render :partial => 'attachments/form', :locals => {:container => issue}
+    when "user"
+      assignable_users = (issue.project.assignable_users.to_a + [issue.project.default_assigned_to]).uniq.compact
+      blank_option = content_tag('option', "--- #{l(:actionview_instancetag_blank_option)} ---", :value => '')
+      select_tag(field_name, blank_option + principals_options_for_select(assignable_users, custom_value.value))
+    when "version"
+      blank_option = content_tag('option', "--- #{l(:actionview_instancetag_blank_option)} ---", :value => '')
+      select_tag(field_name, blank_option + version_options_for_select(issue.assignable_versions, custom_value.value))
     when "date"
-      text_field_tag(field_name, custom_value.value, tag_options.merge(:size => 10)) +
-      calendar_for(field_id)
+      date_field_tag(field_name, custom_value.value, tag_options.merge(:size => 10)) +
+          calendar_for(field_id)
     when "text"
       text_area_tag(field_name, custom_value.value, tag_options.merge(:rows => 4, :cols => 65, :style => "width:auto; resize:both;"))
     when "bool"
       custom_value.custom_field.format.edit_tag self,
-        field_id,
-        field_name,
-        custom_value,
-        :class => "#{custom_value.custom_field.field_format}_cf"
+                                                field_id,
+                                                field_name,
+                                                custom_value,
+                                                :class => "#{custom_value.custom_field.field_format}_cf"
     when "list"
     when "enumeration"
       blank_option = ''.html_safe
@@ -180,7 +191,7 @@ module InlineIssuesHelper
         end
       end
       s = select_tag(field_name, blank_option + options_for_select(custom_field.possible_values_options(custom_value.customized), custom_value.value),
-        tag_options.merge(:multiple => custom_field.multiple?))
+                     tag_options.merge(:multiple => custom_field.multiple?, :id => (issue.id.to_s + '_' + custom_field.name.to_s + '_enum').parameterize.underscore))
       if custom_field.multiple?
         s << hidden_field_tag(field_name, '')
       end
@@ -189,5 +200,5 @@ module InlineIssuesHelper
       text_field_tag(field_name, custom_value.value, tag_options)
     end
   end
-  
+
 end
--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_inline_edit_issues do
   name 'Inline Edit Issues plugin'
   author 'Tomasz Gietek for Omega Code Sp. z o.o.'
   description 'This is a plugin for Redmine.  It allows inline edit of issues in the issues index page.'
-  version '2.0.0'
+  version '2.0.1'
 
   requires_redmine :version_or_higher => '2.0.0'
 
